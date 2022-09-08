e4206bb2fe26e6727ce2598b39209d449d4f31de
Maksym Sarabun <sarabunmax@urk.net>
2022-09-08 19:31:26 +0300

    Working version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from bs4 import BeautifulSoup
 import requests as req
 import re
-from bs4 import BeautifulSoup
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 URL = 'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/bmw' \
       '/?currency=UAH&search%5Bfilter_enum_model%5D%5B0%5D=7-series '
@@ -9,50 +10,53 @@ URL = 'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/bmw' \
 
 # function for getting html page
 def get_html(url):
-
     resp = req.get(url).text
     return resp
 
 
 # function for extract data from html page
 def data_parsing(data_page):
-
     soup = BeautifulSoup(data_page, 'lxml')
     links = list(soup.find_all('a', class_='css-1bbgabe'))  # storage for saving links
-    count_link = 1  # count link in 'links'
-    auto = {}  # storage for saving data auto
+    auto = []  # storage for saving data auto
 
     for link in links:
         html_car = req.get('https://www.olx.ua' + link.get('href')).text
         soup = BeautifulSoup(html_car, 'lxml')
-        data = {
-            'Release date': '',
-            'Price': ''
-        }
+        price_tag = soup.find('h3', class_='css-okktvh-Text eu5v0x0')
 
-        for d in soup.find_all('p', class_='css-xl6fe0-Text eu5v0x0'):
-            if re.search(r'Рік випуску: .*', d.text):
-                data['Release date'] = re.search(r'\d\d\d\d', d.text).group(0)
+        if price_tag is not None and re.search(r'\d', price_tag.text) is not None:
+            data = {'Release date': '', 'Price': price_tag.text}
 
-        if soup.find('h3', class_='css-okktvh-Text eu5v0x0').text is not None:
-            data['Price'] = soup.find('h3', class_='css-okktvh-Text eu5v0x0').text
-        else:
-            data['Price'] = '-'
+            for d in soup.find_all('p', class_='css-xl6fe0-Text eu5v0x0'):
+                if re.search(r'Рік випуску: .*', d.text):
+                    data['Release date'] = re.search(r'\d\d\d\d', d.text).group(0)
 
-        auto[count_link] = data
-        count_link = count_link + 1
+            auto.append(data)
     return auto
 
 
-def gistograme(data: dict):
-    x = set()
-    for d in data.keys():
-        x.add(int(data[d]['Release date']))
-    print(x)
+# function for painting analytic gistogram
+def gistograme(data: list):
+    dates = set(int(v['Release date']) for v in data)
+    price = dict.fromkeys(dates)
 
+    for k in price.keys():
+        price[k] = []
+
+    for value in data:
+
+        if '$' in value['Price'] or '€' in value['Price']:
+            price[int(value['Release date'])].append(int(re.search(r'\d+', value['Price'].replace(' ', '')).group()))
+        else:
+            price[int(value['Release date'])].append(
+                int(re.search(r'\d+', value['Price'].replace(' ', '')).group()) / 37)
+
+    for key, value in price.items():
+        price[key] = int(np.average(value))
+
+    plt.bar(price.keys(), price.values())
+    plt.show()
 
 autos = data_parsing(get_html(URL))
-# print(autos)
 gistograme(autos)
-
-
