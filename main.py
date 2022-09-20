41acbde032e7687ed4fc5b102112505841e59507
Maksym Sarabun <sarabunmax@ukr.net>
2022-09-20 13:58:13 +0300

    Version 1.2

--- main.py
+++ main.py
@@ -1,75 +1,37 @@
-from bs4 import BeautifulSoup
-import requests as req
-import re
-import matplotlib.pyplot as plt
-import numpy as np
-
-URL = 'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/bmw' \
-      '/?currency=UAH&search%5Bfilter_enum_model%5D%5B0%5D=7-series'
+import UserConfig as UC
+import DataProcessor as DP
 
 
-# function for getting html page
-def get_html(url):
-    resp = req.get(url).text
-    return resp
+config = UC.Config()  # object container with configuration parametrs
+URL = f'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/{config.mark}/?currency=USD&search%5Bfilter_enum_model' \
+      f'%5D%5B0%5D={config.model}'  # URL link for parsing
 
+try:
+    html_page = DP.get_html(URL)  # html code recived from web page
+    print('Почалась обробка данних. Будь ласка зачекайте завершення...')
+    vechicle_data = []  # storage for saving clean data
 
-# function for extract data from html page
-def html_parsing(html_page, data_stoarage: list):
-    soup = BeautifulSoup(html_page, 'lxml')
-    links = list(soup.find_all('a', class_='css-1bbgabe'))  # storage for saving links
+    # block variables for checking last page
+    page_numb = 1
+    autos_info = []
+    last_size = 1
 
-    for link in links:
-        html_car = req.get('https://www.olx.ua' + link.get('href')).text
-        soup = BeautifulSoup(html_car, 'lxml')
-        price_tag = soup.find('h3', class_='css-okktvh-Text eu5v0x0')
+    # cycle for processing each web page
+    while True:
 
-        if price_tag is not None and re.search(r'\d', price_tag.text) is not None:
-            data = {'Release date': '', 'Price': price_tag.text}
-
-            for d in soup.find_all('p', class_='css-xl6fe0-Text eu5v0x0'):
-                if re.search(r'Рік випуску: .*', d.text):
-                    data['Release date'] = re.search(r'\d\d\d\d', d.text).group(0)
-
-            data_stoarage.append(data)
-
-
-# function for painting analytic gistogram
-def gistograme(data: list):
-    dates = set(int(v['Release date']) for v in data)
-    price = dict.fromkeys(dates)
-
-    for k in price.keys():
-        price[k] = []
-
-    for value in data:
-
-        if '$' in value['Price'] or '€' in value['Price']:
-            price[int(value['Release date'])].append(int(re.search(r'\d+', value['Price'].replace(' ', '')).group()))
+        if len(autos_info) != last_size:
+            last_size = len(autos_info)
+            DP.html_parsing(html_page, vechicle_data)
+            page_numb += 2
         else:
-            price[int(value['Release date'])].append(
-                int(re.search(r'\d+', value['Price'].replace(' ', '')).group()) / 37)
-    # print(price)
-    # for key, value in price.items():
-    #     price[key] = int(np.average(value))
-    #
-    # plt.bar(price.keys(), price.values())
-    # plt.show()
+            break
+
+    DP.write_data(vechicle_data, config)  # writing clean data ti csv file
+    DP.gistograme(vechicle_data, config)  # building plot for price analyze
+    print('Обробка данних завершена успішно. Файли згенеровані.')
+
+except ConnectionError:
+    print('Помилка з\'єднання з сервером. Перевірте правильність данних конфігурації та спробуйте ще раз.')
+    exit()
 
 
-page_numb = 1
-autos_info = []
-last_size = 1
-# html_parsing(get_html(f'{URL}&page={page_numb}'), autos_info)
-
-while True:
-
-    if len(autos_info) != last_size:
-        last_size = len(autos_info)
-        html_parsing(get_html(f'{URL}&page={page_numb}'), autos_info)
-        page_numb += 1
-    else:
-        break
-
-print(len(autos_info))
-#gistograme(autos_info)
